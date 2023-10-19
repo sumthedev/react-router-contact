da660f07204511b0abd1dcf6853fdd83902ab8cb
Sumbal Hashmi <115552450+sumthedev@users.noreply.github.com>
2023-10-19 18:52:53 +0500

    reinitialized files

---
+++ src/main.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { action as destroyAction } from "./routes/destroy";
+
+import * as ReactDOM from "react-dom/client";
+import EditContact, { action as editAction } from "./routes/edit";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import Index from "./routes/index";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+
+            action: destroyAction,
+          },
+        ],
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/contact.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useRef } from "react";
+import { Form, useFetcher, useLoaderData, useLocation } from "react-router-dom";
+import { getContacts, createContact } from "../contact";
+
+export function loader({ params }) {
+  return getContacts(params.contactId);
+}
+
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true" ? true : false,
+  });
+}
+
+export default function ContactRoute() {
+  const contact = useLoaderData();
+  return <Contact contact={contact} />;
+}
+
+export function Contact({ contact }) {
+  const location = useLocation();
+  const headingRef = useRef();
+
+  useEffect(() => {
+    if (location.state?.takeFocus) {
+      headingRef.current?.focus();
+    }
+  }, [location]);
+
+  return (
+    <div id="contact">
+      <div>
+        <img key={contact.avatar} src={contact.avatar || null} />
+      </div>
+
+      <div>
+        <h1 ref={headingRef} tabIndex="-1">
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter && (
+          <p>
+            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        )}
+
+        {contact.notes && <p>{contact.notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={(event) => {
+              if (!confirm("Please confirm you want to delete this record.")) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Favorite({ contact }) {
+  const fetcher = useFetcher();
+  let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true" ? true : false;
+  }
+
+  return (
+    <fetcher.Form method="post">
+      <button
+        name="favorite"
+        value={favorite ? "false" : "true"}
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </fetcher.Form>
+  );
+}
